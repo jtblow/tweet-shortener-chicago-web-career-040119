8ac96d794b5c951bcf222f61701ff070264d2c54
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-25 17:54:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,5 +38,12 @@ def selective_tweet_shortener(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    puts "#{word_substituter(tweet)[1..137]}..."
+  end
+end
+  
   
      
